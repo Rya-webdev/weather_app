fb818825bcc4608650a1b350f5db9845b723648f
rya-webdev <brokenfreakinradio@gmail.com>
2022-07-24 10:31:22 +0430

    sth innit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e68a588977msh77137136c016bf9p1ac9a9jsnb794c1725761",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
